e89ce2a70a0eabef30aa444a92363eaaa61b23fd
= <=>
2024-02-17 23:25:48 +0530

    set

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,6 @@
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-r from-purple-600 via-purple-700 to-white h-96 ">
-      Your content here
-    </div>
+    <div className="bg-gradient-to-r from-purple-600 via-purple-700 to-white h-96 "></div>
   );
 };
 
